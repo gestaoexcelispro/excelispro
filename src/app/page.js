307a3183fea2b7b41dcc751226aca635368141f6
gestaoexcelispro <gestao.excelispro@gmail.com>
2026-08-12 06:06:56 -0300

    Create page.js

---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '40px', fontFamily: 'sans-serif', textAlign: 'center' }}>
+      <h1>ExcelisPro</h1>
+      <p>Construction Takeoff & Lean Planning Services</p>
+    </main>
+  );
+}
