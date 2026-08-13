34c68dff4c793cf7592431cabcd8e28491f074b2
gestaoexcelispro <gestao.excelispro@gmail.com>
2026-08-13 16:29:53 -0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
+      <head>
+        {/* Importação oficial do CSS do Leaflet para alinhar os blocos do mapa */}
+        <link 
+          rel="stylesheet" 
+          href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" 
+          integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" 
+          crossOrigin="" 
+        />
+      </head>
       <body style={{ margin: 0, padding: 0, boxSizing: 'border-box', fontFamily: 'sans-serif' }}>
         
         {/* O provedor de idioma envolve todo o site e liga a rede */}
