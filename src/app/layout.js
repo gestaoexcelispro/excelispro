7805a35f3624ee3796104131bcd67924a04da52b
gestaoexcelispro <gestao.excelispro@gmail.com>
2026-08-13 09:40:49 -0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,21 @@
+import { LanguageProvider } from '../contexts/LanguageContext';
+
+export const metadata = {
+  title: 'ExcelisPro System',
+  description: 'Sistema Interno de Gestão e Propostas - ExcelisPro',
+};
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="pt-BR">
+      <body style={{ margin: 0, padding: 0, boxSizing: 'border-box', fontFamily: 'sans-serif' }}>
+        
+        {/* O provedor de idioma envolve todo o site e liga a rede */}
+        <LanguageProvider>
+          {children}
+        </LanguageProvider>
+        
+      </body>
     </html>
   );
 }
